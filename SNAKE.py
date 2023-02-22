23850008595e3b9a3c17a9fadc86ba005e918a77
hacbit <107751081+hacbit@users.noreply.github.com>
2023-02-22 12:58:02 +0800

    Add files via upload

--- SNAKE.py
+++ SNAKE.py
@@ -1,8 +1,9 @@
 import time
 import random
 
+
 class Snake:
-    def __init__(self, __velocity, __bodyAxis = [[1, 1], [1, 2]]):
+    def __init__(self, __velocity):
         self.__length = 2
         self.__nextSteAxis = 'r'
         self.__bodyAxis = [[1, 1], [1, 2]]
@@ -10,15 +11,21 @@ class Snake:
 
     def isCollide(self):
         return self.__nextSteAxis in self.__bodyAxis[1:-1]
+
+    def isInBody(self, sth):
+        return sth in self.__bodyAxis
     
     def crawl(self):
         pass
 
-    def eat(self):
-        pass
+    def lengthGrow(self):
+        self.__length += 1
 
-    def score(self):
-        print("Score: ", self.__length - 2, end='')
+    def getLength(self):
+        return self.__length
+
+    def getHeadAxis(self):
+        return self.__bodyAxis[0]
 
 
 class Place:
@@ -40,10 +47,36 @@ class Place:
         
         print('--'*self.__placeWidth + '---')
 
-    
+    def setSth(self, axis, sth):
+        self.__placeData[axis[0]][axis[1]] = sth[0] + ' '
+
+    def getWidth(self):
+        return self.__placeWidth
+
+    def getHeight(self):
+        return self.__placeHeight
+
+
+class Game(Snake, Place):
+    def __init__(self):
+        self.__foodAxis = [5, 5]
+
+    def __setRandomAxis(self):
+        axis = []
+        axis.append(random.randint(0, self.getWidth-1),
+                    random.randint(0, self.getHeight-1))
+        return axis
+
+    def put(self, sth):
+        dic = {'food':'$',
+            'obstacle':'#'}
+        axis = self.__setRandomAxis
+        if not self.isInBody and sth in dic:
+            self.setSth(axis, dic[sth])
+
 
 if __name__ == '__main__':
     snake = Snake(1)
     p = Place()
-    p.initPlace()
-    p.LoadPlace()
+
+
