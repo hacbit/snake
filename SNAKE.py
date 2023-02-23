b9da2f6fb842f882e6e3615d12680df459584434
hacbit <107751081+hacbit@users.noreply.github.com>
2023-02-23 23:46:43 +0800

    Add files via upload

--- SNAKE.py
+++ SNAKE.py
@@ -1,13 +1,14 @@
 import time
 import random
+import os
 
 
 class Snake:
-    def __init__(self, __velocity):
+    def __init__(self):
         self.__length = 2
         self.__nextSteAxis = 'r'
         self.__bodyAxis = [[1, 1], [1, 2]]
-        self.__velocity = __velocity
+        #self.__velocity = __velocity
 
     def isCollide(self):
         return self.__nextSteAxis in self.__bodyAxis[1:-1]
@@ -35,6 +36,7 @@ class Place:
         self.__placeData = []
 
     def initPlace(self):
+        self.__placeData = []
         for _ in range(self.__placeHeight + 2):
             self.__placeData.append('  ' * self.__placeWidth)
 
@@ -45,7 +47,7 @@ class Place:
         for i in self.__placeData:
             print('| ' + i + '|')
         
-        print('--'*self.__placeWidth + '---')
+        print('--'*self.__placeWidth + '---', end='')
 
     def setSth(self, axis, sth):
         self.__placeData[axis[0]][axis[1]] = sth[0] + ' '
@@ -56,23 +58,31 @@ class Place:
     def getHeight(self):
         return self.__placeHeight
 
+# -*-*-*-*-*-
 
-class Game(Snake, Place):
-    def __setRandomAxis(self):
-        axis = []
-        axis.append(random.randint(0, self.getWidth-1),
-                    random.randint(0, self.getHeight-1))
-        return axis
+def setRandomAxis(place):
+    return [random.randint(0, place.getWidth()-1),
+            random.randint(0, place.getHeight()-1)]
 
-    def put(self, sth):
-        dic = {'food':'$',
-            'obstacle':'#'}
-        axis = self.__setRandomAxis
-        if not self.isInBody and sth in dic:
-            self.setSth(axis, dic[sth])
+def set_food0rObsta(self, snake, ch):
+    dic = {'food':'$',
+        'obstacle':'#'}
+    axis = setRandomAxis()
+    if not snake.isInBody() and ch in dic:
+        snake.setSth(axis, dic[ch])
+
+def putScore(snake):
+    score = snake.getLength()-2
+    print('----------GAMEOVER----------')
+    print('YOUR SCORE IS ', score, ' pt')
+
+def CLEAR():
+    os.system('cls')
 
 
 if __name__ == '__main__':
-    pass
+    p = Place()
+    s = Snake()
 
-
+    #print(s.getLength())
+    #print(setRandomAxis(p))
