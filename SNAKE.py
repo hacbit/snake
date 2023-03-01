2be03683301dab2351ef2be942dd1a1d1fac291a
hacbit <107751081+hacbit@users.noreply.github.com>
2023-03-01 20:25:59 +0800

    Add files via upload
    用curses重新写了一部分逻辑

--- SNAKE.py
+++ SNAKE.py
@@ -1,83 +1,89 @@
-from os import system
+__author__ = 'hacbit'
+__doc__ = '''
+Copyright (C) 2023- hacbit
+'''
+
+from os import system, name
+from sys import stdout
 from time import sleep
 from numpy import sum
 from random import randint
-from keyboard import wait
 from pynput.keyboard import Key, Listener, GlobalHotKeys
-
+import curses
 
 class Snake:
-    def __init__(self, __velocity = 3, __placeWidth = 20, __placeHeight = 20):
-        self.__velocity = __velocity
-        self.__placeData = []
+    def __init__(self, __level = 3, __placeWidth = 20, __placeHeight = 20):
+        self.__level = __level
+        self.__time = self.__setLevel()
         self.__snakeDirection = 'r'
         self.__bodyAxis = [[1, 1], [1, 2]]
         self.__placeWidth = __placeWidth
         self.__placeHeight = __placeHeight
+        self.__placeData = self.__initPlace()
         self.__snakeDirectionDict = {
                 'l':[-1,0],
                 'r':[1,0],
                 'u':[0,1],
                 'd':[0,-1]
             }
+        
+        
+    def __setLevel(self):
+        return [0.4, 0.2, 0.1][self.__level-1]
 
-    def initPlace(self):
-        self.__placeData = []
+    def __initPlace(self):
+        arr = []
         for _ in range(self.__placeHeight + 2):
-            self.__placeData.append('  ' * self.__placeWidth)
+            arr.append('  ' * self.__placeWidth)
+        return arr
 
     def LoadPlace(self):
         print(' ' + '--'*self.__placeWidth + '-')
         for i in self.__placeData:
             print('| ' + i + '|')
-        print(' ' + '--'*self.__placeWidth + '-', end='')
+        print(' ' + '--'*self.__placeWidth + '-')
     
     def crawl(self):
-        nextAxis = self.addAxis(self.__bodyAxis[0], 
+        nextAxis = self.__addAxis(self.__bodyAxis[0], 
                                 self.__snakeDirection)
         self.__bodyAxis = nextAxis + \
-            self.__bodyAxis[:self.getLength()+self.isEat()-1]
-
-    def setGameLevel(self):
-        arr = [1, 2, 3, ]
+            self.__bodyAxis[:self.getLength()+self.__isEat()-1]
 
     def putScore(self):
         score = self.getLength()-2
         print('----------GAMEOVER----------')
         print('YOUR SCORE IS ', score, ' pt')
 
-    def draw(self, sth, axis):
+    def __draw(self, sth, axis):
         dic = {'food':'$',
                 'obstacle':'#',
                 'head':'@',
                 'trunk':'*'
             }
-        if not self.isInBody() and sth in dic:
+        if not self.__isInBody() and sth in dic:
             self.__placeData[axis[0]][axis[1]] = sth[0] + ' '
 
-    def getRandomAxis(self):
+    def __getRandomAxis(self):
         return [randint(0, self.getWidth()-1),
                 randint(0, self.getHeight()-1)]
 
-    def addAxis(self, axis, direction):
+    def __addAxis(self, axis, direction):
         return sum([axis, direction], axis=0).tolist()
 
-    def nextStep(self):
+    def __nextStep(self):
         if self.__snakeDirection in self.__snakeDirectionDict:
             return self.__snakeDirectionDict[self.__snakeDirection]
     
-    def isInBody(self, sth):
+    def __isInBody(self, sth):
         return sth in self.__bodyAxis
     
-    def isCollide(self):
-        return self.nextStep() in self.__bodyAxis[1:-1]
+    def __isCollide(self):
+        return self.__nextStep() in self.__bodyAxis[1:-1]
 
-    def isEat(self):
+    def __isEat(self):
         return self.__placeData[self.__bodyAxis[0][0]]\
                                 [self.__bodyAxis[0][1]] == '$'
     
-
-# -*-*-*-*-*-*-api-*-*-*-*-*-
     def getLength(self):
         return len(self.__bodyAxis)
     
@@ -90,29 +96,31 @@ class Snake:
     def getHeight(self):
         return self.__placeHeight
 
-    def Direction_change(self):
-        pass
-
-
-def CLEAR():
-    system('cls')
-
-keylist = [Key.left, Key.right, Key.up, Key.down, 
-           'a', 'd', 'w', 's']
-
-def on_press(key):
-    try:
-        if key in keylist or key.char in keylist:
-            pass
-    except:
-        return
+    def Direction_change(self, key):
+        return self.__snakeDirectionDict[key]
     
-def on_release(key):
-    if key == Key.esc:
-        return False
+# ===== class Snake end =====
 
+keylist = ['l', 'r', 'u', 'd', 
+           'a', 'd', 'w', 's',
+           Key.left, Key.right, Key.up, Key.down]
+direction = curses.KEY_RIGHT
 isStop = False
 level = 3
+
+def CLEAR():
+    system('cls' if name == 'nt' else 'clear')
+
+def on_press(key):
+    global direction
+    if key == 'a' and not direction == curses.RIGHT:
+        direction = curses.LEFT
+    elif key == 'd' and not direction == curses.LEFT:
+        direction = curses.RIGHT
+    elif key == 'w' and not direction == curses.DOWN:
+        direction = curses.UP
+    elif key == 's' and not direction == curses.UP:
+        direction = curses.DOWN
 
 def actionAfterHotKey(key):
     ls = ['1st', '2nd', '3rd']
@@ -121,38 +129,38 @@ def actionAfterHotKey(key):
     isStop = True
     level = key
 
-def put1by1(str, t, end='\n'):
-    for i in range(len(str)):
-        print(str[:i], end='\r')
-        sleep(t)
-    print(str, end=end)
-
 
 if __name__ == '__main__':
-    with Listener(on_press=on_press, 
-                  on_release=on_release,
-                  suppress=True) as listener:
+    screen = curses.initscr()
+    curses.curs_set(0)  # hide the cursor
+    listener = Listener(on_press=on_press)
+    listener.start()
+    screen.addstr(0, 0, "-----WELCOME TO SNAKE-----")
+    screen.addstr(1, 0, "using 'W A S D' or '↑ ↓ ← →' to contorl")
+    screen.addstr(2, 0, 'press enter to continue ......')
+    screen.refresh()
+    s = screen.getstr(3, 0)     # wait before press enter
+    screen.addstr(10, 0, "you input "+str(s))
+    screen.addstr(4, 0, 'Now choose your game level (1.EASY  2.MEDIUM  3.HARD)')
+    screen.refresh()
+    '''
+    with GlobalHotKeys({
+        '1': lambda:actionAfterHotKey(1),
+        '2': lambda:actionAfterHotKey(2),
+        '3': lambda:actionAfterHotKey(3)}) as h:
+        while not isStop:
+            pass
+        h.stop()
 
-        CLEAR()
-        put1by1("-----WELCOME TO SNAKE-----", 0.02)
-        put1by1("using 'W A S D' or '↑ ↓ ← →' to contorl", 0.02)
-        put1by1('press enter to continue', 0.02, end='\r')
-        for i in range(6):
-            print('press ENTER to continue', '.'*(i+1), end='\r')
-            sleep(0.15)
-        wait('enter')
+    game = Snake(level)
 
-        print('Now choose your game level (1.EASY  2.MEDIUM  3.HARD)')
-        with GlobalHotKeys({
-            '1': lambda:actionAfterHotKey(1),
-            '2': lambda:actionAfterHotKey(2),
-            '3': lambda:actionAfterHotKey(3)}) as h:
-            while not isStop:
-                pass
-            h.stop()
-
-        game = Snake(level)
-        game.initPlace()
-        game.LoadPlace()
-
-        listener.join()
+    sleep(1)
+    game.putScore()'''
+    listener.stop()
+    
+    screen.clear()
+    screen.addstr(0, 0, '-----GAMEOVER-----')
+    screen.addstr(1, 0, 'press any key to exit program')
+    screen.refresh()
+    screen.getch()
+    curses.endwin()
